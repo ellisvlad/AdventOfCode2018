47580d413feef6dfc89cb50a42099fb11fb550d1
Vlad Ellis <me@ellisvlad.com>
2018-12-03 13:38:09 +0000

    Change how Main works

--- src/com/ellisvlad/aoc2018/Main.java
+++ src/com/ellisvlad/aoc2018/Main.java
@@ -5,12 +5,25 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import com.ellisvlad.aoc2018.day1.ChallengeMain;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+	
+	private static Map<Integer, Class<? extends DayChallenge>> challengeClzs = new HashMap<>();
+	private static int SOLVED_DAYS;
 
-	private static int SOLVED_DAYS = 1;
+	static {
+		for (int day = 1; SOLVED_DAYS == 0; day++) {
+			try {
+				Class<?> clz = Class.forName("com.ellisvlad.aoc2018.day" + day + ".ChallengeMain");
+
+				challengeClzs.put(day, (Class<? extends DayChallenge>) clz);
+			} catch (ClassNotFoundException e) {
+				SOLVED_DAYS = day - 1;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		int pickedDay = 0;
@@ -51,7 +64,8 @@ public class Main {
 		BufferedReader inputFile = getInput(pickedDay);
 
 		try {
-			new ChallengeMain().runChallenge(inputFile);
+			DayChallenge challenge = challengeClzs.get(pickedDay).newInstance();
+			challenge.runChallenge(inputFile);
 		} catch (Exception e) {
 			System.err.println("Failed to run challenge for day " + pickedDay);
 			e.printStackTrace();
